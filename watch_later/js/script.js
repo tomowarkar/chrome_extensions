10fd32db5c1d2e475df4d024a7923bb051e62c4d
tomowarkar <3104kawamoto@gmail.com>
2019-12-09 08:32:55 +0900

    refactor: copyToClipboard

--- watch_later/js/script.js
+++ watch_later/js/script.js
@@ -94,28 +94,6 @@ const swapObject = (dragStatus) => {
   init(favikes);
 }
 
-// クリップボードにコピーするための関数
-const copyToClipboard = str => {
-  // https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
-  const el = document.createElement('textarea');
-  el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  document.body.appendChild(el);
-  const selected =
-    document.getSelection().rangeCount > 0
-      ? document.getSelection().getRangeAt(0)
-      : false;
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-  if (selected) {
-    document.getSelection().removeAllRanges(); 
-    document.getSelection().addRange(selected);
-  }
-};
-
 // メイン処理
 if (window.localStorage.favike == undefined) {
   window.localStorage.favike = '[]';
